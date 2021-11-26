eb0887993bbd82e39a5ad7a05b34f9ece88607a9
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-11-26 10:28:27 +0100

    add Header and maxWidhtWrapper components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
-import { GlobalStyles } from "../globalStyles";
+import backgroundLight from "../../public/images/bg-mobile-light.jpg";
 import styled from "styled-components";
+
+import { GlobalStyles } from "../globalStyles";
+import { Header } from "./Header";
+import { MaxWidthWrapper } from "./MaxWidthWrapper";
 
 function App() {
   return (
     <Wrapper>
-      <GlobalStyles />
+      <MaxWidthWrapper>
+        <Header />
+        <GlobalStyles />
+      </MaxWidthWrapper>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  min-height: 100%;
+  background-color: var(--very-light-gray);
+  background-image: url(${backgroundLight});
+  background-size: 100% 200px;
+  background-repeat: no-repeat;
+  padding-top: 50px;
+`;
+
 export default App;
