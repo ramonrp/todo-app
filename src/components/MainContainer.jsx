c222b088cac1b48a34ef2a22eeac92d2943a3a73
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-01 17:42:23 +0100

    add useLocalStorageState hook

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,16 +1,20 @@
 import * as React from "react";
 import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
 import { Input } from "./Input";
 import { FilterList } from "./FilterList";
 import { ListWrapper } from "./ListWrapper";
+import { QUERIES } from "../constants";
+import { useLocalStorageState } from "../hooks";
 
 const MainContainer = () => {
-  const [list, setList] = React.useState([]);
+  const [list, setList] = useLocalStorageState("tasks", []);
   const [filter, setFilter] = React.useState("non filter");
 
   function addElement(e) {
     e.preventDefault();
     const task = e.target.elements.textInput.value;
+    if (task === "") return;
     e.target.elements.textInput.value = "";
     const newList = [...list, { id: uuidv4(), task, completed: false }];
     setList(newList);
@@ -32,6 +36,11 @@ const MainContainer = () => {
     setList(newList);
   }
 
+  function removeFinishTask() {
+    const newList = list.filter((task) => !task.completed);
+    setList(newList);
+  }
+
   const onDragEnd = (result) => {
     const { destination, source, draggableId } = result;
     if (!destination) return;
@@ -48,18 +57,46 @@ const MainContainer = () => {
   };
 
   return (
-    <>
+    <Wrapper>
       <Input addElement={addElement}></Input>
+      <Spacer />
       <ListWrapper
         list={list}
         taskToggler={taskToggler}
         filter={filter}
         removeElement={removeElement}
         onDragEnd={onDragEnd}
+        removeFinishTask={removeFinishTask}
+        setFilter={setFilter}
       />
-      <FilterList setFilter={setFilter} />
-    </>
+      <Spacer />
+      <FilterListWrapper>
+        <FilterList setFilter={setFilter} filter={filter} />
+      </FilterListWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  font-size: 12px;
+  @media ${QUERIES.tabletAndUp} {
+    font-size: ${18 / 18}rem;
+  }
+`;
+const Spacer = styled.div`
+  margin-bottom: 16px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-bottom: 24px;
+  }
+`;
+
+const FilterListWrapper = styled.div`
+  display: initial;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: none;
+  }
+`;
 
 export { MainContainer };
